faf7b49d946f26a8dd7dbafce5f8d30d0783da50
Florian <florianpaulschoene@gmail.com>
2023-04-13 11:46:27 +0200

    scrol on click bugfix

--- portfolio-webapp/src/components/Home/Home.tsx
+++ portfolio-webapp/src/components/Home/Home.tsx
@@ -4,25 +4,22 @@ import './Home.css'
 import WelcomeText from './WelcomeText'
 import LatestProjects from './LatestProjects'
 import ParticleBackground from '../ParticleBackground/ParticleBackground'
-import Animation from './Animation'
 import About from '../About/About'
 import Contact from '../Contact/Contact'
+import LandingPage from '../LandingPage/LandingPage'
 
-export default function Home({ scrollAboutRef }: any) {
-	const scrollContactRef = useRef<HTMLInputElement>()
-	const scrollAboutRefs = useRef(scrollAboutRef)
-	console.log(scrollAboutRefs)
+export default function Home({ aboutRef, projectsRef, landingRef }: any) {
+	const contactRef = useRef<HTMLInputElement>()
+
+	console.log(aboutRef)
 
 	return (
 		<div className='home'>
 			<ParticleBackground />
-			<div className='home--landing'>
-				<WelcomeText scrollContactRef={scrollContactRef} />
-				<Animation />
-			</div>
-			<About scrollAboutRef={scrollAboutRef} />
-			<LatestProjects />
-			<Contact scrollContactRef={scrollContactRef} />
+			<LandingPage contactRef={contactRef} landingRef={landingRef} />
+			<About aboutRef={aboutRef} />
+			<LatestProjects projectsRef={projectsRef} />
+			<Contact contactRef={contactRef} />
 		</div>
 	)
 }

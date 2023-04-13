09771608747e4a0b8c6de1b146a3de30714eed45
Florian <florianpaulschoene@gmail.com>
2023-04-13 14:09:03 +0200

    path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
+import App from '../src/components/App/App'
 import './assets/styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 
